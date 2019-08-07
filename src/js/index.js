bf671df8d99c946367b84ed3183e7083c26d242d
Bartosz <bzhk911@gmail.com>
2019-08-07 19:38:13 +0200

    init daftplayer

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../style/main.scss";
+
+const Index = () => {
+  return <h1>Hello Reasdact!</h1>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: {
+    main: "./src/js/index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
