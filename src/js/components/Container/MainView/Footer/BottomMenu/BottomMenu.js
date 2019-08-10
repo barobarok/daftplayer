04de15111e3e717d19024bd3bc5ef48a8d871ffb
Bartosz <bzhk911@gmail.com>
2019-08-10 03:30:27 +0200

    render bottom menu

--- src/js/components/Container/MainView/Footer/BottomMenu/BottomMenu.js
+++ src/js/components/Container/MainView/Footer/BottomMenu/BottomMenu.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import SVG from "react-inlinesvg";
+import Icon from "./playlist_ico.svg";
 const BottomMenu = ({}) => {
-  return <div className="bottomMenu__container" />;
+  return (
+    <div className="bottomMenu__container">
+      <div className="bottomMenu__icon">
+        <SVG src={Icon} />
+      </div>
+      <div className="bottomMenu__info">
+        <div className="bottomMenu__info--next">
+          <p className="bottomMenu__info--next-label">NEXT</p>
+          <p className="bottomMenu__info--next-title">Bobowa piosenka</p>
+          {/* ograniczenie ilosci znakow */}
+        </div>
+        <div className="bottomMenu__info--empty">........</div>
+        <div className="bottomMenu__info--time">2:23</div>
+      </div>
+    </div>
+  );
 };
 
 export default BottomMenu;
